7ee3c799e896772b8f0c98e21b6a7b2a835c8a00
really-lilly <tatkalillian@gmail.com>
2022-01-18 14:19:56 -0800

    Clean data and sort into test/train

--- classifier.py
+++ classifier.py
@@ -1,24 +1,12 @@
 from oscillatorDB import mongoMethods as mm
 import tellurium as te
 import pandas as pd
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout
-from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers import Adam
-
-from sklearn.metrics import classification_report,confusion_matrix
-
-import tensorflow as tf
-
-import cv2
-import os
-
 import re
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import pandas as pd
 import numpy as np
 
 # Load data
@@ -57,6 +45,8 @@ for i in range(len(oscillators["jacobian"])):
     try:
         j = oscillators["jacobian"][i]
         j = convert_jac_string(j)
+        if len(j) != 9:
+            continue
         osc.append(j)
     except Exception as e:
         print(e)
@@ -65,66 +55,40 @@ for i in range(len(notOsc["jacobian"])):
     try:
         j = notOsc["jacobian"][i]
         j = convert_jac_string(j)
+        if len(j) != 9:
+            continue
         nonOsc.append(j)
     except Exception as e:
         print(e)
 
-j_train = []
-label_train = []
-j_test = []
-label_test = []
-
-
+j = []
+label = []
 
 for i in range(len(osc)):
-    j_test.append(osc[i])
-    label_test.append(True)
+    j.append(osc[i])
+    label.append(True)
 
-for i in range(1000):
-    j_train.append(nonOsc[i])
-    label_train.append(False)
+for i in range(len(nonOsc)):
+    j.append(nonOsc[i])
+    label.append(False)
+#
+X_train, X_test, y_train, y_test = train_test_split(j, label, train_size = 0.80, random_state = 100)
 
-for i in range(1000, len(nonOsc)):
-    j_test.append(nonOsc[i])
-    label_test.append(False)
+# our model instance
+model = LogisticRegression()
 
-datagen = ImageDataGenerator(featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range = 0,  # randomly rotate images in the range (degrees, 0 to 180)
-        zoom_range = 0, # Randomly zoom image
-        width_shift_range=0,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip = False,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
-#
-datagen.fit(j_train)
+# train the classifier
+model.fit(X_train, y_train)
 
-# model = Sequential()
-# opt = Adam(lr=0.0001)
-# model.compile(optimizer=opt,loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True) , metrics = ['accuracy'])
-#
-# history = model.fit(j_train,label_train,epochs = 500 , validation_data = (j_test, label_test))
-#
-# acc = history.history['accuracy']
-# val_acc = history.history['val_accuracy']
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
-#
-# epochs_range = range(500)
-#
-# plt.figure(figsize=(15, 15))
-# plt.subplot(2, 2, 1)
-# plt.plot(epochs_range, acc, label='Training Accuracy')
-# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-# plt.legend(loc='lower right')
-# plt.title('Training and Validation Accuracy')
-#
-# plt.subplot(2, 2, 2)
-# plt.plot(epochs_range, loss, label='Training Loss')
-# plt.plot(epochs_range, val_loss, label='Validation Loss')
-# plt.legend(loc='upper right')
-# plt.title('Training and Validation Loss')
-# plt.show()
+# use trained model from above to predict the class of "new" data
+predicted = model.predict(X_test)
+
+sum = 0
+for i in range(len(predicted)):
+    if predicted[i] == y_test[i]:
+        sum += 1
+
+
+print(sum)
+# # let's see how well the classifier performed
+# print(r2_score(y_test, predicted))
