e4ec65393d63144e91b703e7f320a15e01d992e7
really-lilly <tatkalillian@gmail.com>
2022-01-18 11:39:41 -0800

    Extract and process jacobians

--- classifier.py
+++ classifier.py
@@ -9,7 +9,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
 
 from sklearn.metrics import classification_report,confusion_matrix
 
@@ -18,36 +18,113 @@ import tensorflow as tf
 import cv2
 import os
 
+import re
 import numpy as np
 
-print("Done")
+# Load data
+oscillators = pd.read_csv("/home/hellsbells/Desktop/oscillator_jacobains.csv")
+notOsc = pd.read_csv("/home/hellsbells/Desktop/non-oscillator_jacobians.csv")
+
+osc = []
+nonOsc = []
+
+def convert_jac_string(j):
+    j = re.sub('\s{1,}', '$', j)
+    j = j[1:-1]
+    if j.startswith("$"):
+        j = j[1:]
+    if j.endswith("$"):
+        j = j[:-1]
+    j = j.split("$")
+
+    newJ = []
+    for num in j:
+        if 'e' in num:
+            if '+' in num:
+                num, e = num.split('e')
+                e = e[1:]
+                newJ.append(float(num) * 10 ** (float(e)))
+            elif '-' in num:
+                num, e = num.split('e')
+                e = e[1:]
+                newJ.append(float(num) * 10 ** (-1 * float(e)))
+        else:
+            newJ.append(float(num))
+    return newJ
+
+# Hacky bullshit to convert strings into list of floats
+for i in range(len(oscillators["jacobian"])):
+    try:
+        j = oscillators["jacobian"][i]
+        j = convert_jac_string(j)
+        osc.append(j)
+    except Exception as e:
+        print(e)
+
+for i in range(len(notOsc["jacobian"])):
+    try:
+        j = notOsc["jacobian"][i]
+        j = convert_jac_string(j)
+        nonOsc.append(j)
+    except Exception as e:
+        print(e)
+
+j_train = []
+label_train = []
+j_test = []
+label_test = []
+
+
+
+for i in range(len(osc)):
+    j_test.append(osc[i])
+    label_test.append(True)
+
+for i in range(1000):
+    j_train.append(nonOsc[i])
+    label_train.append(False)
+
+for i in range(1000, len(nonOsc)):
+    j_test.append(nonOsc[i])
+    label_test.append(False)
+
+datagen = ImageDataGenerator(featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range = 0,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0, # Randomly zoom image
+        width_shift_range=0,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip = False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
 #
-# query = {"num_nodes": 3, "oscillator": True}
-# result = mm.query_database(query)
+datagen.fit(j_train)
+
+# model = Sequential()
+# opt = Adam(lr=0.0001)
+# model.compile(optimizer=opt,loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True) , metrics = ['accuracy'])
 #
-# jacobians = []
-# model_ids = []
-# oscillator = []
-# for i in range(10):
-#     model = result[i]["model"]
-#     m = te.loada(model)
-#     model_ids.append(result[i]["ID"])
-#     j = m.getFullJacobian()
-#     jacobians.append(j)
-#     oscillator.append(result[i]["oscillator"])
+# history = model.fit(j_train,label_train,epochs = 500 , validation_data = (j_test, label_test))
 #
-# query = {"num_nodes": 3, "oscillator": False}
-# result = mm.query_database(query)
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
 #
-# for i in range(10):
-#     model = result[i]["model"]
-#     m = te.loada(model)
-#     model_ids.append(result[i]["ID"])
-#     j = m.getFullJacobian()
-#     jacobians.append(j)
-#     oscillator.append(result[i]["oscillator"])
+# epochs_range = range(500)
 #
-# data = {"oscillator": oscillator,
-#         "ID": model_ids,
-#         "jacobian": jacobians}
+# plt.figure(figsize=(15, 15))
+# plt.subplot(2, 2, 1)
+# plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
 #
+# plt.subplot(2, 2, 2)
+# plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+# plt.show()
--- loadData.py
+++ loadData.py
@@ -2,45 +2,49 @@ from oscillatorDB import mongoMethods as mm
 import tellurium as te
 import pandas as pd
 
-query = {"num_nodes": 3, "oscillator": True}
-result = mm.query_database(query)
+query = {"num_nodes": 3, "oscillator": False, "simulates": True}
+result, length = mm.query_database(query, returnLength=True)
 
 
-## Load and write out oscillators
-jacobians = []
-model_ids = []
-oscillator = []
-for i in range(10):
+
+# for i in range(length):
+#     r = te.loada(result[i]["model"])
+#     try:
+#         r.simulate()
+#         newVal = {"simulates": True}
+#         mm.update_model({"ID": result[i]["ID"]}, newVal)
+#     except Exception:
+#         newVal = {"simulates": False}
+#         mm.update_model({"ID": result[i]["ID"]}, newVal)
+
+
+
+#
+# # ## Load and write out oscillators
+# jacobians = []
+# model_ids = []
+# oscillator = []
+
+for i in range(835, length):
     model = result[i]["model"]
     m = te.loada(model)
-    model_ids.append(result[i]["ID"])
-    j = m.getFullJacobian()
-    j = j.flatten()
-    jacobians.append(j)
-    oscillator.append(result[i]["oscillator"])
+    try:
+        j = m.getFullJacobian()
+        j = j.flatten()
+        data = {"jacobian": [j],
+                "oscillator": [False]}
+        oscillators = pd.DataFrame(data=data)
+        oscillators.to_csv(path_or_buf="/home/hellsbells/Desktop/non-oscillator_jacobians.csv", mode='a', header=False)
+    except Exception:
+        continue
 
-data = {"oscillator": oscillator,
-        "ID": model_ids,
-        "jacobian": jacobians}
-
-oscillators = pd.DataFrame(data=data)
-
-oscillators.to_csv("/home/hellsbells/Desktop/oscillator_jacobains.csv")
-
-#
-# ## Load and write out non-oscillators
-# query = {"num_nodes": 3, "oscillator": False}
-# result = mm.query_database(query)
-#
-# for i in result.count():
-#     model = result[i]["model"]
-#     m = te.loada(model)
-#     model_ids.append(result[i]["ID"])
-#     j = m.getFullJacobian()
-#     jacobians.append(j)
-#     oscillator.append(result[i]["oscillator"])
+print("done")
 #
 # data = {"oscillator": oscillator,
 #         "ID": model_ids,
 #         "jacobian": jacobians}
 #
+# oscillators = pd.DataFrame(data=data)
+#
+# oscillators.to_csv(path_or_buf="/home/hellsbells/Desktop/non-oscillator_jacobians.csv")
+
